c749e08f34e978889c5e750347663c1415733ec1
roy <yulongroy@gmail.com>
2025-05-21 09:21:26 +0800

    2

--- detectron2/modeling/roi_heads/clip_roi_heads.py
+++ detectron2/modeling/roi_heads/clip_roi_heads.py
@@ -27,6 +27,308 @@ from .mask_head import build_mask_head
 
 from .roi_heads import ROI_HEADS_REGISTRY, select_foreground_proposals, ROIHeads
 
+class _ScaleGradient(Function):
+    @staticmethod
+    def forward(ctx, input, scale):
+        ctx.scale = scale
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output * ctx.scale, None
+
+
+@ROI_HEADS_REGISTRY.register()
+class Cascade_CLIPRes5ROIHeads(ROIHeads):
+    """
+    Created for CLIP ResNet. This head uses the last resnet layer from backbone.
+    Extended from Res5ROIHeads in roi_heads.py
+    """
+
+    @configurable
+    def __init__(
+        self,
+        *,
+        in_features: List[str],
+        pooler: ROIPooler,
+        res5: None,
+        box_predictors: List[nn.Module],
+        mask_head: Optional[nn.Module] = None,
+        proposal_matchers: List[Matcher],
+        **kwargs,
+    ):
+        """
+        NOTE: this interface is experimental.
+
+        Args:
+            in_features (list[str]): list of backbone feature map names to use for
+                feature extraction
+            pooler (ROIPooler): pooler to extra region features from backbone
+            res5 (nn.Sequential): a CNN to compute per-region features, to be used by
+                ``box_predictor`` and ``mask_head``. Typically this is a "res5"
+                block from a ResNet.
+            box_predictor (nn.Module): make box predictions from the feature.
+                Should have the same interface as :class:`FastRCNNOutputLayers`.
+            mask_head (nn.Module): transform features to make mask predictions
+        """
+        kwargs["proposal_matcher"] = proposal_matchers[0]
+        super().__init__(**kwargs)
+        self.in_features = in_features
+        self.pooler = pooler
+        # if isinstance(res5, (list, tuple)):
+        #     res5 = nn.Sequential(*res5)
+        self.res5 = res5  #  None, this head uses the res5 from backbone
+        self.box_predictors = nn.ModuleList(box_predictors)
+        self.proposal_matchers = proposal_matchers
+        self.mask_on = mask_head is not None
+        if self.mask_on:
+            self.mask_head = mask_head
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        # fmt: off
+        ret = super().from_config(cfg)
+        ret.pop("proposal_matcher")
+        cascade_bbox_reg_weights = [
+            [10.0, 10.0, 5.0, 5.0],  # 第一阶段的边界框回归权重
+            [20.0, 20.0, 10.0, 10.0]  # 第二阶段的边界框回归权重
+        ]
+        cascade_ious = [0.5, 0.6]
+        in_features = ret["in_features"] = cfg.MODEL.ROI_HEADS.IN_FEATURES
+        pooler_resolution = cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION
+        pooler_type       = cfg.MODEL.ROI_BOX_HEAD.POOLER_TYPE
+        pooler_scales     = (1.0 / input_shape[in_features[0]].stride, )
+        sampling_ratio    = cfg.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO
+        mask_on           = cfg.MODEL.MASK_ON
+        # fmt: on
+        assert not cfg.MODEL.KEYPOINT_ON
+        assert len(in_features) == 1
+
+        ret["pooler"] = ROIPooler(
+            output_size=pooler_resolution,
+            scales=pooler_scales,
+            sampling_ratio=sampling_ratio,
+            pooler_type=pooler_type,
+        )
+
+        # Compatbility with old moco code. Might be useful.
+        # See notes in StandardROIHeads.from_config
+        # if not inspect.ismethod(cls._build_res5_block):
+        #     logger.warning(
+        #         "The behavior of _build_res5_block may change. "
+        #         "Please do not depend on private methods."
+        #     )
+        #     cls._build_res5_block = classmethod(cls._build_res5_block)
+        ret["res5"], out_channels = None, cfg.MODEL.RESNETS.RES2_OUT_CHANNELS * 8  # cls._build_res5_block(cfg)
+        box_predictors, proposal_matchers = [],[]
+        for match_iou, bbox_reg_weights in zip(cascade_ious, cascade_bbox_reg_weights):
+            box_predictors.append(
+                FastRCNNOutputLayers(
+                    cfg,
+                    ShapeSpec(channels=out_channels, height=1, width=1),
+                    box2box_transform=Box2BoxTransform(weights=bbox_reg_weights),
+                )
+            )
+            proposal_matchers.append(Matcher([match_iou], [0, 1], allow_low_quality_matches=False))
+
+        ret["box_predictors"] = box_predictors
+        ret["proposal_matchers"] = proposal_matchers
+        if mask_on:
+            ret["mask_head"] = build_mask_head(
+                cfg,
+                ShapeSpec(channels=out_channels, width=pooler_resolution, height=pooler_resolution),
+            )
+        return ret
+
+    def _shared_roi_transform(self, features, boxes, backbone_res5):
+        x = self.pooler(features, boxes)
+        return backbone_res5(x)
+
+    def forward(self, images, features, proposals, targets=None, res5=None,attnpool=None):
+        """
+        See :meth:`ROIHeads.forward`.
+        """
+        del images
+
+        if self.training:
+            assert targets
+            proposals = self.label_and_sample_proposals(proposals, targets)
+        #del targets
+
+        att_heads = nn.ModuleList()
+        a = attnpool
+        for i in range(2):
+            att_heads.append(a)
+        head_outputs = []
+        prev_pred_boxes = None  # 存储上一个阶段的预测边界框。
+        image_sizes = [x.image_size for x in proposals]
+        for k in range(2):
+            if k > 0:
+                # The output boxes of the previous stage are used to create the input
+                # proposals of the next stage.
+                # 如果不是第一个阶段(k > 0)，使用上一个阶段的预测边界框生成当前阶段的输入提议。
+                proposals = self._create_proposals_from_boxes(prev_pred_boxes, image_sizes)
+                # 如果处于训练模式，则对这些提议进行匹配和标记。
+                if self.training:
+                    proposals = self._match_and_label_boxes(proposals, k, targets)
+            proposal_boxes = [x.proposal_boxes for x in proposals]
+            box_features = self._shared_roi_transform(
+                [features[f] for f in self.in_features], proposal_boxes, res5
+            )
+            box_features = _ScaleGradient.apply(box_features, 1.0 / 2)
+            att_feat = att_heads[k](box_features)
+            predictions = self.box_predictors[k](att_feat) # 执行当前阶段的预测。
+            prev_pred_boxes = self.box_predictors[k].predict_boxes(predictions, proposals) #更新为当前阶段预测的边界框。
+            head_outputs.append((self.box_predictors[k], predictions, proposals)) #添加当前阶段的预测器、预测结果和提议
+
+        if self.training:
+            del features
+            losses = {}
+            storage = get_event_storage()
+            for stage, (predictor, predictions, proposals) in enumerate(head_outputs):
+                with storage.name_scope("stage{}".format(stage)):
+                    stage_losses = predictor.losses(predictions, proposals)
+                losses.update({k + "_stage{}".format(stage): v for k, v in stage_losses.items()})
+            if self.mask_on:
+                proposals, fg_selection_masks = select_foreground_proposals(
+                    proposals, self.num_classes
+                )
+                # Since the ROI feature transform is shared between boxes and masks,
+                # we don't need to recompute features. The mask loss is only defined
+                # on foreground proposals, so we need to select out the foreground
+                # features.
+                mask_features = box_features[torch.cat(fg_selection_masks, dim=0)]
+                del box_features
+                losses.update(self.mask_head(mask_features, proposals))
+            return [], losses
+        else:
+            # Each is a list[Tensor] of length #image. Each tensor is Ri x (K+1)
+            scores_per_stage = [h[0].predict_probs(h[1], h[2]) for h in head_outputs]
+
+            # Average the scores across heads
+            # 对于每个图像，将不同阶段的得分（对应于同一个图像）相加并除以级联阶段的总数self.num_cascade_stages，
+            # 以计算得分的平均值。这样做是为了融合各个阶段的预测，提高最终预测的准确性。
+            scores = [
+                sum(list(scores_per_image)) * (1.0 / 2)
+                for scores_per_image in zip(*scores_per_stage)
+            ]
+            scores_bf_multiply = scores
+            # Use the boxes of the last head
+            predictor, predictions, proposals = head_outputs[-1]
+            boxes = predictor.predict_boxes(predictions, proposals)
+            pred_instances, _ = fast_rcnn_inference(
+                boxes,
+                scores,
+                image_sizes,
+                predictor.test_score_thresh,
+                predictor.test_nms_thresh,
+                predictor.soft_nms_enabled,
+                predictor.soft_nms_method,
+                predictor.soft_nms_sigma,
+                predictor.soft_nms_prune,
+                predictor.test_topk_per_image,
+                scores_bf_multiply=scores_bf_multiply,
+                vis=False
+            )
+            pred_instances = self.forward_with_given_boxes(features, pred_instances, res5)
+            return pred_instances, {}
+
+    def forward_with_given_boxes(self, features, instances, res5=None):
+        """
+        Use the given boxes in `instances` to produce other (non-box) per-ROI outputs.
+
+        Args:
+            features: same as in `forward()`
+            instances (list[Instances]): instances to predict other outputs. Expect the keys
+                "pred_boxes" and "pred_classes" to exist.
+
+        Returns:
+            instances (Instances):
+                the same `Instances` object, with extra
+                fields such as `pred_masks` or `pred_keypoints`.
+        """
+        assert not self.training
+        assert instances[0].has("pred_boxes") and instances[0].has("pred_classes")
+
+        if self.mask_on:
+            features = [features[f] for f in self.in_features]
+            x = self._shared_roi_transform(features, [x.pred_boxes for x in instances], res5)
+            return self.mask_head(x, instances)
+        else:
+            return instances
+
+    @torch.no_grad()
+    def _match_and_label_boxes(self, proposals, stage, targets):
+        """
+        Match proposals with groundtruth using the matcher at the given stage.
+        Label the proposals as foreground or background based on the match.
+
+        Args:
+            proposals (list[Instances]): One Instances for each image, with
+                the field "proposal_boxes".
+            stage (int): the current stage
+            targets (list[Instances]): the ground truth instances
+
+        Returns:
+            list[Instances]: the same proposals, but with fields "gt_classes" and "gt_boxes"
+        """
+        num_fg_samples, num_bg_samples = [], []
+        for proposals_per_image, targets_per_image in zip(proposals, targets):
+            match_quality_matrix = pairwise_iou(
+                targets_per_image.gt_boxes, proposals_per_image.proposal_boxes
+            )
+            # proposal_labels are 0 or 1
+            matched_idxs, proposal_labels = self.proposal_matchers[stage](match_quality_matrix)
+            if len(targets_per_image) > 0:
+                gt_classes = targets_per_image.gt_classes[matched_idxs]
+                # Label unmatched proposals (0 label from matcher) as background (label=num_classes)
+                gt_classes[proposal_labels == 0] = self.num_classes
+                gt_boxes = targets_per_image.gt_boxes[matched_idxs]
+            else:
+                gt_classes = torch.zeros_like(matched_idxs) + self.num_classes
+                gt_boxes = Boxes(
+                    targets_per_image.gt_boxes.tensor.new_zeros((len(proposals_per_image), 4))
+                )
+            proposals_per_image.gt_classes = gt_classes
+            proposals_per_image.gt_boxes = gt_boxes
+
+            num_fg_samples.append((proposal_labels == 1).sum().item())
+            num_bg_samples.append(proposal_labels.numel() - num_fg_samples[-1])
+
+        # Log the number of fg/bg samples in each stage
+        storage = get_event_storage()
+        storage.put_scalar(
+            "stage{}/roi_head/num_fg_samples".format(stage),
+            sum(num_fg_samples) / len(num_fg_samples),
+        )
+        storage.put_scalar(
+            "stage{}/roi_head/num_bg_samples".format(stage),
+            sum(num_bg_samples) / len(num_bg_samples),
+        )
+        return proposals
+
+    def _create_proposals_from_boxes(self, boxes, image_sizes):
+        """
+        Args:
+            boxes (list[Tensor]): per-image predicted boxes, each of shape Ri x 4
+            image_sizes (list[tuple]): list of image shapes in (h, w)
+
+        Returns:
+            list[Instances]: per-image proposals with the given boxes.
+        """
+        # Just like RPN, the proposals should not have gradients
+        boxes = [Boxes(b.detach()) for b in boxes]
+        proposals = []
+        for boxes_per_image, image_size in zip(boxes, image_sizes):
+            boxes_per_image.clip(image_size)
+            if self.training:
+                # do not filter empty boxes at inference time,
+                # because the scores from each stage need to be aligned and added later
+                boxes_per_image = boxes_per_image[boxes_per_image.nonempty()]
+            prop = Instances(image_size)
+            prop.proposal_boxes = boxes_per_image
+            proposals.append(prop)
+        return proposals
 
 
 @ROI_HEADS_REGISTRY.register()
